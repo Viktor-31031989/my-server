74f54dc473929739e213d87ea5716b6b61b92e56
viktor <viktordikovich@gmail.com>
2022-11-03 22:39:00 -0500

    new server

--- Index.js
+++ Index.js
@@ -1,27 +1,91 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = 3000;
+const Joi = require('joi')
 
 const cars = [
-    {id:1, brand: "BMW", year: "2017", price: "20000" },
-    {id:2, brand: "audi", year: "2015", price: "30232"},
-    {id:3, brand: "opel", year: "2010" , price: "32435"},
-    {id:3, brand: "lada", year: "2019", price: "500" },
+    {id: 1, pic: "🚙", brand: "BMW", year: 2017, price: 20000, color: "blue", title: "used"},
+    {id: 2, pic: "🚗", brand: "audi", year: 2015, price: 30232, color: "red", title: "used"},
+    {id: 3, pic: "🚚", brand: "opel", year: 2010, price: 32435, color: "green", title: "used"},
+    {id: 4, pic: "🏎", brand: "lada", year: 2002, price: 500, color: "red", title: "used"},
+    {id: 5, pic: "🚘", brand: "lexus", year: 2022, price: 74000, color: "white", title: "new"}
 ]
 
 app.use(express.json())
 
-app.get('/api/cars', (req,res) =>{
+//VALIDATE FUNCTION
+function validateCar(car) {
+    const schema = Joi.object({
+        brand: Joi.string().min(3).required(),
+        year: Joi.number().min(4).required(),
+        price: Joi.number().min(3).required(),
+        pic: Joi.string().required(),
+        color: Joi.string().required(),
+        title: Joi.string().required()
+    });
+
+    return schema.validate(car);
+}
+
+//GET CARS
+app.get('/cars', (req, res) => {
     res.send(cars)
 })
 
-app.post('/api/cars', (req, res)=>{
+//ADD CAR
+app.post('/cars', (req, res) => {
+
+    //Validate with Joi npm i joi
+    const {error} = validateCar(req.body)
+
+    if (error) return res.status(400).send(error.details[0].message);
+
     const car = {
         id: cars.length + 1,
-        brand: req.body.brand
+        brand: req.body.brand,
+        year: req.body.year,
+        price: req.body.price,
+        pic: req.body.pic,
+        color: req.body.color,
+        title: req.body.title
     };
     cars.push(car);
     res.send(car)
+});
+
+//UPDATE
+app.put('/cars/:id', (req, res) => {
+    //Look up the cars
+    const car = cars.find(c => c.id === parseInt(req.params.id));
+    if (!car) return res.status(404).send('The course with the given ID was not found')
+
+    //Validate
+    const {error} = validateCar(req.body)
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Update cars
+
+    car.brand = req.body.brand
+    car.pic = req.body.pic
+    car.price = req.body.price
+    car.year = req.body.year
+    car.color = req.body.color
+    car.title = req.body.title
+    res.send(car)
+})
+
+//DELETE
+app.delete('/cars/:id', (req, res) =>{
+    const car = cars.find(c => c.id === parseInt(req.params.id));
+    if (!car) return  res.status(404).send('The course with the given ID was not found')
+
+    //Delete
+    const index = cars.indexOf(car);
+    cars.splice(index, 1);
+
+    res.send(car);
+
 })
 
 
